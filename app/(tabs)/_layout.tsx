ffeb4d384910f16046412545e9d537809826a7bc
Darren Pottinger <darren.pottinger@gmail.com>
2023-10-09 17:38:07 -0400

    Creates Pokedex

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -50,6 +50,13 @@ export default function TabLayout() {
           tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
         }}
       />
+      <Tabs.Screen
+        name="pokedex"
+        options={{
+          title: 'Pokedex',
+          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+        }}
+      />
     </Tabs>
   );
 }
