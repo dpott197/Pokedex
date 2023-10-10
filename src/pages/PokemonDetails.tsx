0bf6b893fe2b28fc6393a06fada6e9691914b5df
Darren Pottinger <darren.pottinger@gmail.com>
2023-10-10 18:51:08 -0400

    Update colors

--- src/pages/PokemonDetails.tsx
+++ src/pages/PokemonDetails.tsx
@@ -1,9 +1,10 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { capitalizeFirstLetter } from '../utils/utils';
+import { capitalizeFirstLetter, getTypeColor } from '../utils/utils';
 
 interface PokemonData {
     name: string;
+    types: { type: { name: string } }[];
     sprites: {
       front_default: string;
     };
@@ -24,19 +25,25 @@ function PokemonDetails() {
   }
   
   return (
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+    <div style={{ 
+      display: 'flex', 
+      justifyContent: 'center', 
+      alignItems: 'center', 
+      height: '100vh', 
+      backgroundColor: 'black'
+    }}>
       <div className="pokemon-details" style={{ 
         width: '300px', 
         padding: '20px', 
         boxShadow: '0 4px 8px rgba(0,0,0,0.1)', 
         borderRadius: '10px', 
-        backgroundColor: 'rgba(255, 255, 255, 0.1)'  /* Slightly transparent background */
+        backgroundColor: getTypeColor(pokemonDetails.types[0].type.name)
       }}>
         <h2 style={{ fontSize: '24px', fontWeight: 'bold', marginBottom: '20px' }}>{capitalizeFirstLetter(pokemonDetails.name)}</h2>
         <img src={pokemonDetails.sprites.front_default} alt={pokemonDetails.name} style={{ width: '100%', borderRadius: '50%', boxShadow: '0 4px 8px rgba(0,0,0,0.1)' }} />
       </div>
     </div>
-  );  
+  );
 }
 
 export default PokemonDetails;
