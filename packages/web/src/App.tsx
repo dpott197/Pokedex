f521532e6956e3193e6f94c0175c8af889dd2dc0
Darren Pottinger <darren.pottinger@gmail.com>
2023-10-10 12:49:03 -0400

    Refactoring

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,68 +1,9 @@
-import React, { useState, useEffect, useRef } from 'react';
+
 import './App.css';
-
-interface Pokemon {
-  name: string;
-  url: string;
-}
-
-function capitalizeFirstLetter(string: string) {
-  return string.charAt(0).toUpperCase() + string.slice(1);
-}
-
-// Source: https://www.pokemon.com/us/pokedex
-const TOTAL_POKEMON = 1010
+import PokemonList from './PokemonList';
 
 function App() {
-  const [pokemonList, setPokemonList] = useState<Pokemon[]>([]);
-  const [currentPage, setCurrentPage] = useState(1);
-  const [searchTerm, setSearchTerm] = useState('');
-  const itemsPerPage = TOTAL_POKEMON;
-
-  const listRef = useRef<HTMLDivElement>(null);
-
-  useEffect(() => {
-    // Reset the list when the search term changes
-    setPokemonList([]);
-    setCurrentPage(1);
-  }, [searchTerm]);
-
-  useEffect(() => {
-    fetch(`https://pokeapi.co/api/v2/pokemon?limit=${itemsPerPage}&offset=${(currentPage - 1) * itemsPerPage}`)
-      .then(response => response.json())
-      .then(data => {
-        const filteredPokemon = data.results.filter((pokemon: Pokemon) => pokemon.name.includes(searchTerm.toLowerCase()));
-        setPokemonList(prev => [...prev, ...filteredPokemon]);
-      });
-  }, [currentPage, searchTerm]);
-
-  // ... (rest of the imports and states)
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Pokémon List</h1>
-        <input
-          type="text"
-          placeholder="Search Pokémon..."
-          value={searchTerm}
-          onChange={e => setSearchTerm(e.target.value)}
-        />
-          <div className="grid-container">
-            {pokemonList.map(pokemon => (
-              <div key={pokemon.name} className="pokemon-item">
-                <img
-                  src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.url.split('/')[6]}.png`}
-                  alt={pokemon.name}
-                  className="pokemon-image"
-                />
-                <span>{capitalizeFirstLetter(pokemon.name)}</span>
-              </div>
-            ))}
-          </div>
-      </header>
-    </div>
-  );
+  return PokemonList();
 }
 
 export default App;
---
+++ packages/web/src/PokemonList.tsx
@@ -0,0 +1,59 @@
+import { useState, useEffect } from 'react';
+import './App.css';
+import { Pokemon } from './interface/Pokemon';
+import { capitalizeFirstLetter } from './utils';
+
+// Source: https://www.pokemon.com/us/pokedex
+const TOTAL_POKEMON = 1010
+
+function PokemonList() {
+  const [pokemonList, setPokemonList] = useState<Pokemon[]>([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [searchTerm, setSearchTerm] = useState('');
+  const itemsPerPage = TOTAL_POKEMON;
+
+  useEffect(() => {
+    // Reset the list when the search term changes
+    setPokemonList([]);
+    setCurrentPage(1);
+  }, [searchTerm]);
+
+  useEffect(() => {
+    fetch(`https://pokeapi.co/api/v2/pokemon?limit=${itemsPerPage}&offset=${(currentPage - 1) * itemsPerPage}`)
+      .then(response => response.json())
+      .then(data => {
+        const filteredPokemon = data.results.filter((pokemon: Pokemon) => pokemon.name.includes(searchTerm.toLowerCase()));
+        setPokemonList(prev => [...prev, ...filteredPokemon]);
+      });
+  }, [currentPage, searchTerm]);
+
+  // ... (rest of the imports and states)
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Pokémon List</h1>
+        <input
+          type="text"
+          placeholder="Search Pokémon..."
+          value={searchTerm}
+          onChange={e => setSearchTerm(e.target.value)}
+        />
+          <div className="grid-container">
+            {pokemonList.map(pokemon => (
+              <div key={pokemon.name} className="pokemon-item">
+                <img
+                  src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.url.split('/')[6]}.png`}
+                  alt={pokemon.name}
+                  className="pokemon-image"
+                />
+                <span>{capitalizeFirstLetter(pokemon.name)}</span>
+              </div>
+            ))}
+          </div>
+      </header>
+    </div>
+  );
+}
+
+export default PokemonList;
