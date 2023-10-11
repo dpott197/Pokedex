15be29dff713f115f42118298282c73be27cc4d4
Darren Pottinger <darren.pottinger@gmail.com>
2023-10-11 12:35:09 -0400

    Shows stats

--- src/containers/PokemonDetail.tsx
+++ src/containers/PokemonDetail.tsx
@@ -3,12 +3,20 @@ import { useParams } from 'react-router-dom';
 import { capitalizeFirstLetter } from '../utils/capitalizeFirstLetter';
 import { getTypeColor } from '../utils/getTypeColor';
 
-interface PokemonData {
+interface Stat {
+  base_stat: number;
+  stat: {
     name: string;
-    types: { type: { name: string } }[];
-    sprites: {
-      front_default: string;
-    };
+  };
+}
+
+interface PokemonData {
+  name: string;
+  types: { type: { name: string } }[];
+  sprites: {
+    front_default: string;
+  };
+  stats: Stat[]; // Add this line
 }
 
 function PokemonDetail() {
@@ -24,7 +32,7 @@ function PokemonDetail() {
   if (!pokemonDetails) { 
     return <div>Loading...</div>;
   }
-  
+
   return (
     <div style={{ 
       display: 'flex', 
@@ -42,6 +50,18 @@ function PokemonDetail() {
       }}>
         <h2 style={{ fontSize: '24px', fontWeight: 'bold', marginBottom: '20px' }}>{capitalizeFirstLetter(pokemonDetails.name)}</h2>
         <img src={pokemonDetails.sprites.front_default} alt={pokemonDetails.name} style={{ width: '100%', borderRadius: '50%', boxShadow: '0 4px 8px rgba(0,0,0,0.1)' }} />
+        
+        {/* Render the stats */}
+        <div style={{ marginTop: '20px' }}>
+          <h3>Stats:</h3>
+          <ul>
+            {pokemonDetails.stats.map(stat => (
+              <li key={stat.stat.name}>
+                {capitalizeFirstLetter(stat.stat.name)}: {stat.base_stat}
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </div>
   );
