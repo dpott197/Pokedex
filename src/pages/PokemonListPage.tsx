9847adc32c5032f061bb40233798e3d6adf7959a
Darren Pottinger <darren.pottinger@gmail.com>
2023-10-11 09:29:17 -0400

    Save searchHistory. Only search on enter.

--- src/pages/PokemonListPage.tsx
+++ src/pages/PokemonListPage.tsx
@@ -1,21 +1,28 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react'; // Import useState
 import './../App.css';
 import { capitalizeFirstLetter } from '../utils/utils';
 import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-import { fetchPokemonList, setSearchTerm } from './../redux/slices/pokemonSlice'; // Import the actions and selectors
+import { fetchPokemonList, setSearchTerm } from './../redux/slices/pokemonSlice';
 import { AppDispatch, RootState } from '../redux/store';
 
 function PokemonList() {
+  const [localSearchTerm, setLocalSearchTerm] = useState(''); // Local state for the search input
   const dispatch = useDispatch<AppDispatch>();
   const pokemonList = useSelector((state: RootState) => state.pokemon.list);
   const status = useSelector((state: RootState) => state.pokemon.status);
   const searchTerm = useSelector((state: RootState) => state.pokemon.searchTerm);
+  const searchHistory = useSelector((state: RootState) => state.pokemon.searchHistory);
 
   useEffect(() => {
-    // Fetch the list when the component mounts
     dispatch(fetchPokemonList({ limit: 1010, offset: 0 }));
   }, [dispatch]);
+
+  const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      dispatch(setSearchTerm(localSearchTerm)); // Update the Redux state when Enter is pressed
+    }
+  };
 
   return (
     <div className="App">
@@ -24,25 +31,34 @@ function PokemonList() {
         <input
           type="text"
           placeholder="🔍 Search Pokémon..."
-          value={searchTerm}
-          onChange={e => dispatch(setSearchTerm(e.target.value))}
+          value={localSearchTerm}
+          onChange={e => setLocalSearchTerm(e.target.value)} // Update the local state on every change
+          onKeyPress={handleKeyPress} // Check for "Enter" key press
         />
-      <div className="grid-container">
-        {status === 'loading' && <p>Loading...</p>}
-        {status === 'succeeded' && pokemonList.filter(pokemon => pokemon.name.includes(searchTerm.toLowerCase())).map(pokemon => (
-          <Link to={`/pokemon/${pokemon.url.split('/')[6]}`} key={pokemon.url.split('/')[6]}>
-            <div className="pokemon-item">
-              <img
-                src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.url.split('/')[6]}.png`}
-                alt={pokemon.name}
-                className="pokemon-image"
-              />
-              <span>{capitalizeFirstLetter(pokemon.name)}</span>
-            </div>
-          </Link>
-        ))}
-        {status === 'failed' && <p>Error fetching data</p>}
-      </div>
+        <div className="search-history">
+          <h3>Recent Searches:</h3>
+          <ul>
+            {searchHistory.map((term, index) => (
+              <li key={index}>{term}</li>
+            ))}
+          </ul>
+        </div>
+        <div className="grid-container">
+          {status === 'loading' && <p>Loading...</p>}
+          {status === 'succeeded' && pokemonList.filter(pokemon => pokemon.name.includes(searchTerm.toLowerCase())).map(pokemon => (
+            <Link to={`/pokemon/${pokemon.url.split('/')[6]}`} key={pokemon.url.split('/')[6]}>
+              <div className="pokemon-item">
+                <img
+                  src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.url.split('/')[6]}.png`}
+                  alt={pokemon.name}
+                  className="pokemon-image"
+                />
+                <span>{capitalizeFirstLetter(pokemon.name)}</span>
+              </div>
+            </Link>
+          ))}
+          {status === 'failed' && <p>Error fetching data</p>}
+        </div>
       </header>
     </div>
   );
